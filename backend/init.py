d7f6fb72105942168fc04f4d4cb1db3beb8267c0
tannergarcia <tannergarcia@ufl.edu>
2023-10-14 19:09:58 -0400

    move function

--- backend/init.py
+++ backend/init.py
@@ -4,9 +4,6 @@ from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 import os, getpass
 
-loader = PyPDFLoader("data/eng-national-retail-9-1-23.pdf")
-pages = loader.load_and_split()
-
 # prompt for API key, only done once at init
 os.environ['OPENAI_API_KEY'] = getpass.getpass('OpenAI API Key:')
 
@@ -18,6 +15,8 @@ if os.path.isdir("data/vecIndex"):
 else:
     option = input("vecIndex does not exist.  Would you like to generate it?  This will use your OpenAPI key. Enter Y to continue: ")
     if option == 'Y':
+        loader = PyPDFLoader("data/eng-national-retail-9-1-23.pdf")
+        pages = loader.load_and_split()
         faiss_index = FAISS.from_documents(pages, OpenAIEmbeddings())
         faiss_index.save_local("data/vecIndex")
     else:
