ef9e4183bff0743908609f52f1bc20725c6f0768
Ekuo79 <ekuo3242@gmail.com>
2023-10-13 23:56:09 -0400

    feat: add conversation route handler

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-# Members API route
-@app.route("/members")
-def members():
-    return {"members": ["Member 1", "Member 2", "Member 3"]}
+@app.route('/')
+def menu():
+    return "Hello World!"
+
+
+# Chatbox API CALL
+@app.route('/home', methods=['POST'])
+def home():
+    #get query string
+    userInput = request.args.get('userInput')
+    print(userInput)
+
+
+    if userInput:
+        #use api call
+        return {"userInput": userInput}
+
+
 
 
 if __name__ == "__main__":
