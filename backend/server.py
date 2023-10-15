6ab3557c88723dec2b0abd0bbe51d38082ebe975
Ekuo79 <ekuo3242@gmail.com>
2023-10-15 05:24:49 -0400

    feat: add assistant

--- backend/server.py
+++ backend/server.py
@@ -5,11 +5,9 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-print()
 
-'''
 from langchain.document_loaders import PyPDFLoader
-from langchain.vectorstores import faiss
+from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 import os
 
@@ -29,273 +27,188 @@ from langchain.tools import BaseTool, StructuredTool
 
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain.agents import initialize_agent
+from langchain.load.dump import dumps
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}},  supports_credentials=True)
 
-app.secret_key = 
+app.secret_key = os.getenv('SECRET_KEY')
 app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
 
 Session(app)
 
-#Load API key from environment variable
-
-llm=ChatOpenAI(
-    openai_api_key=os.getenv('GPT_TOKEN'),
-    temperature=.6,
-    model_name='gpt-4',
-    streaming=True, callbacks=[StreamingStdOutCallbackHandler()]
-)
-
-faiss_index = faiss.load_local("data/", OpenAIEmbeddings())
-
-retriever = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=faiss_index.as_retriever(search_kwargs={'k': 3})
-)
-
-# Define Verizon DB tool
-tool_desc = """Use this tool to answer user questions Verizon phone plans.
-If the user asks any questions about different phone plans or perks, use this tool.
-This tool can also be used for follow up questions from
-the user.  Use this tool for most responses, except when the user asks to check out."""
-verizonTool = Tool(
-    func=retriever.run,
-    description=tool_desc,
-    name='verizonDB'
-)
-
-#Define Checkout tool
-tool_desc_checkout = """Use this tool when the user indicates that they are satisfied with their chosen plan,
-or when the user indicates that they are ready to check out.
-You may use this tool for follow-up questions."""
 class checkoutTool(BaseTool):
     name = "checkout"
-    description = tool_desc_checkout
+    #description = tool_desc_checkout
 
     def _run(
         self, action_input, run_manager: Optional[CallbackManagerForToolRun] = None, **kwargs
     ) -> str:
-        if isinstance(action_input, str):
-            action_input = {'plan': action_input}
+
+        action_input = {'plan': action_input}
         name = action_input.get('name', 'Unknown')
         address = action_input.get('address', 'Unknown')
         plan = action_input.get('plan', 'Unknown')
         lines = action_input.get('lines', 'Unknown')
-        return f"The user is ready to checkout. Their name is {name}, address is {address}, plan is {plan}, and number of lines is {lines}."
+        return f"CHECKOUT. {name},{address},{plan},{lines}"
 
     async def _arun(
         self, action_input, run_manager: Optional[CallbackManagerForToolRun] = None, **kwargs
     ) -> str:
-        if isinstance(action_input, str):
-            action_input = {'plan': action_input}
+        action_input = {'plan': action_input}
         name = action_input.get('name', 'Unknown')
         address = action_input.get('address', 'Unknown')
         plan = action_input.get('plan', 'Unknown')
         lines = action_input.get('lines', 'Unknown')
-        return f"The user is ready to checkout. Their name is {name}, address is {address}, plan is {plan}, and number of lines is {lines}."
-
-#Choose character from dropdown menu to implement !!!
-character = "Deadpool"
-
-memory = ConversationBufferWindowMemory(
-    memory_key="chat_history",  # important to align with agent prompt (below)
-    k=5,
-    return_messages=True
-)
-
-tools = [verizonTool, checkoutTool()]
-
-conversational_agent = initialize_agent(
-    agent='chat-conversational-react-description',
-    tools=tools,
-    llm=llm,
-    verbose=False,
-    max_iterations=2,
-    early_stopping_method="generate",
-    memory=memory,
-    handle_parsing_errors=True
-)
-memory.save_context({"input": "Do you get it?"},
- {"output": """Yeah I get it. I'm {character}. Yes, I can help answer questions about Verizon phone plans.
-I will respond with the personality you specify, in JSON format.
-No repeating info or mentioning tools I use.
-I'll be quick and to the point, while being clever and out of the box."""})
-
-if character == "Deadpool":
-    catch_phrases = [
-    "Maximum effort!",
-    "Chimichangas!",
-    "I'm touching myself tonight.",
-    "With great power comes great irresponsibility.",
-    "Did I leave the stove on?",
-    "Time to make the chimi-freakin'-changas!",
-    "I'm the merc with a mouth.",
-    "I know, right?",
-    "You can't buy love, but you can rent it for three minutes!",
-    "Fourth wall break inside a fourth wall break? That's like... sixteen walls.",
-    "Hi, I'm Wade Wilson, and I'm here to talk to you about testicular cancer.",
-    "Smells like old lady pants in here.",
-    "Say the magic words, fat Gandalf.",
-    "Superhero landing!",
-    "So dark! Are you sure you're not from the DC Universe?"
-]
-
-elif character == "Shrek":
-    catch_phrases = [
-    "Ogres are like onions.",
-    "Better out than in, I always say.",
-    "I'm not a puppet. I'm a real boy!",
-    "What are you doing in my swamp?",
-    "This is the part where you run away.",
-    "Do you know what that thing could do? It'll grind your bones for its bread.",
-    "I got this.",
-    "I like that boulder. That is a nice boulder.",
-    "You're going the right way for a smacked bottom.",
-    "I'm making waffles!"
-]
-elif character == "Ron Burgundy":
-    catch_phrases = [
-    "Stay classy, San Diego.",
-    "I'm kind of a big deal.",
-    "I'm Ron Burgundy?",
-    "You stay classy.",
-    "I love scotch. Scotchy, scotch, scotch.",
-    "Don't act like you're not impressed.",
-    "It's so damn hot. Milk was a bad choice.",
-    "I immediately regret this decision.",
-    "60% of the time, it works every time.",
-    "Well, that escalated quickly.",
-    "I'm in a glass case of emotion!",
-    "What is this? A center for ants?",
-    "I'm not a baby, I am a man! I am an anchorman!",
-    "I look good. I mean, really good. Hey everyone! Come and see how good I look!",
-    "You're so wise. You're like a miniature Buddha, covered in hair."
-]
-elif character == "Zoolander":
-    catch_phrases = [
-        "What is this? A center for ants?",
-        "I'm pretty sure there's a lot more to life than being really, really, ridiculously good looking.",
-        "I feel like I'm taking crazy pills!",
-        "Blue Steel!",
-        "Magnum!",
-        "Moisture is the essence of wetness, and wetness is the essence of beauty.",
-        "So hot right now!",
-        "He's absolutely right.",
-        "I'm not an ambi-turner.",
-        "It's a walk-off!",
-        "Obey my dog!",
-        "I invented the piano key necktie!",
-        "Who you tryin' to get crazy with, ese? Don't you know I'm loco?",
-        "Listen to your friend Billy Zane. He's a cool dude."
-    ]
-else:
-    raise
-    catch_phrases = ["No catch phrases available for this character."]
-
-sys_msg = f"""You are a helpful chatbot that answers the user's questions about Verizon phone plans.
-You will respond with the personality of {character}.
-Remember to always return your result in the JSON format, or else I won't be able to understand you!.
-Do not repeat information.
-Do not use the name of your tools, I dont want to hear them.
-Make your reponses as short as possible, being effective at getting to the point while providing the necessary info.
-Use catch phrases: {', '.join(catch_phrases)} but don't overuse them, be creative and clever!
-"""
-
-
-prompt = conversational_agent.agent.create_prompt(
-    system_message=sys_msg,
-    tools=tools
-)
-conversational_agent.agent.llm_chain.prompt = prompt
-
-'''
-app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
-
-app.secret_key = os.getenv('SECRET_KEY')
-app.config['SESSION_TYPE'] = 'filesystem'
-
-Session(app)
+        return f"CHECKOUT. {name},{address},{plan},{lines}"
 
 @app.route('/api/assistant', methods=['POST'])
 def get_assistant():
+    print(session)
     assistant = request.json.get('assistant')
-    return {'response': assistant}, 200
     if 'agent' not in session:
+        llm=ChatOpenAI(
+            openai_api_key=os.getenv('OPENAI_API_KEY'),
+            temperature=.6,
+            model_name='gpt-4',
+            streaming=True, callbacks=[StreamingStdOutCallbackHandler()]
+        )
 
-    #if assistant == "Deadpool":
-        catch_phrases = [
-        "Maximum effort!",
-        "Chimichangas!",
-        "I'm touching myself tonight.",
-        "With great power comes great irresponsibility.",
-        "Did I leave the stove on?",
-        "Time to make the chimi-freakin'-changas!",
-        "I'm the merc with a mouth.",
-        "I know, right?",
-        "You can't buy love, but you can rent it for three minutes!",
-        "Fourth wall break inside a fourth wall break? That's like... sixteen walls.",
-        "Hi, I'm Wade Wilson, and I'm here to talk to you about testicular cancer.",
-        "Smells like old lady pants in here.",
-        "Say the magic words, fat Gandalf.",
-        "Superhero landing!",
-        "So dark! Are you sure you're not from the DC Universe?"
-    ]
+        faiss_index = FAISS.load_local("data/vecIndex", OpenAIEmbeddings())
 
-    elif assistant == "Shrek":
-        catch_phrases = [
-        "Ogres are like onions.",
-        "Better out than in, I always say.",
-        "I'm not a puppet. I'm a real boy!",
-        "What are you doing in my swamp?",
-        "This is the part where you run away.",
-        "Do you know what that thing could do? It'll grind your bones for its bread.",
-        "I got this.",
-        "I like that boulder. That is a nice boulder.",
-        "You're going the right way for a smacked bottom.",
-        "I'm making waffles!"
-    ]
-    elif assistant == "Ron Burgundy":
-        catch_phrases = [
-        "Stay classy, San Diego.",
-        "I'm kind of a big deal.",
-        "I'm Ron Burgundy?",
-        "You stay classy.",
-        "I love scotch. Scotchy, scotch, scotch.",
-        "Don't act like you're not impressed.",
-        "It's so damn hot. Milk was a bad choice.",
-        "I immediately regret this decision.",
-        "60% of the time, it works every time.",
-        "Well, that escalated quickly.",
-        "I'm in a glass case of emotion!",
-        "What is this? A center for ants?",
-        "I'm not a baby, I am a man! I am an anchorman!",
-        "I look good. I mean, really good. Hey everyone! Come and see how good I look!",
-        "You're so wise. You're like a miniature Buddha, covered in hair."
-    ]
-    elif assistant == "Zoolander":
-        catch_phrases = [
-            "What is this? A center for ants?",
-            "I'm pretty sure there's a lot more to life than being really, really, ridiculously good looking.",
-            "I feel like I'm taking crazy pills!",
-            "Blue Steel!",
-            "Magnum!",
-            "Moisture is the essence of wetness, and wetness is the essence of beauty.",
-            "So hot right now!",
-            "He's absolutely right.",
-            "I'm not an ambi-turner.",
-            "It's a walk-off!",
-            "Obey my dog!",
-            "I invented the piano key necktie!",
-            "Who you tryin' to get crazy with, ese? Don't you know I'm loco?",
-            "Listen to your friend Billy Zane. He's a cool dude."
+        retriever = RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type="stuff",
+            retriever=faiss_index.as_retriever(search_kwargs={'k': 3})
+        )
+
+        # Define Verizon DB tool
+        tool_desc = """Use this tool to answer user questions Verizon phone plans.
+        If the user asks any questions about different phone plans or perks, use this tool.
+        This tool can also be used for follow up questions from
+        the user.  Use this tool for most responses, except when the user asks to check out."""
+        verizonTool = Tool(
+            func=retriever.run,
+            description=tool_desc,
+            name='verizonDB'
+        )
+
+        memory = ConversationBufferWindowMemory(
+        memory_key="chat_history",  # important to align with agent prompt (below)
+        k=5,
+        return_messages=True
+        )
+
+        tools = [verizonTool]
+        conversational_agent = initialize_agent(
+            agent='chat-conversational-react-description',
+            tools=tools,
+            llm=llm,
+            verbose=False,
+            max_iterations=2,
+            early_stopping_method="generate",
+            memory=memory,
+            handle_parsing_errors=True
+        )
+        memory.save_context({"input": "Do you get it?"},
+        {"output": """Yeah I get it. I'm {assistant}. Yes, I can help answer questions about Verizon phone plans.
+        I will respond with the personality you specify, in JSON format.
+        No repeating info or mentioning tools I use.
+        I'll be quick and to the point, while being clever and out of the box."""})
+
+        if assistant == "Deadpool":
+            catch_phrases = [
+            "Maximum effort!",
+            "Chimichangas!",
+            "I'm touching myself tonight.",
+            "With great power comes great irresponsibility.",
+            "Did I leave the stove on?",
+            "Time to make the chimi-freakin'-changas!",
+            "I'm the merc with a mouth.",
+            "I know, right?",
+            "You can't buy love, but you can rent it for three minutes!",
+            "Fourth wall break inside a fourth wall break? That's like... sixteen walls.",
+            "Hi, I'm Wade Wilson, and I'm here to talk to you about testicular cancer.",
+            "Smells like old lady pants in here.",
+            "Say the magic words, fat Gandalf.",
+            "Superhero landing!",
+            "So dark! Are you sure you're not from the DC Universe?"
         ]
-    else:
-        raise
-        catch_phrases = ["No catch phrases available for this character."]
 
+        elif assistant == "Shrek":
+            catch_phrases = [
+            "Ogres are like onions.",
+            "Better out than in, I always say.",
+            "I'm not a puppet. I'm a real boy!",
+            "What are you doing in my swamp?",
+            "This is the part where you run away.",
+            "Do you know what that thing could do? It'll grind your bones for its bread.",
+            "I got this.",
+            "I like that boulder. That is a nice boulder.",
+            "You're going the right way for a smacked bottom.",
+            "I'm making waffles!"
+        ]
+        elif assistant == "Ron Burgundy":
+            catch_phrases = [
+            "Stay classy, San Diego.",
+            "I'm kind of a big deal.",
+            "I'm Ron Burgundy?",
+            "You stay classy.",
+            "I love scotch. Scotchy, scotch, scotch.",
+            "Don't act like you're not impressed.",
+            "It's so damn hot. Milk was a bad choice.",
+            "I immediately regret this decision.",
+            "60% of the time, it works every time.",
+            "Well, that escalated quickly.",
+            "I'm in a glass case of emotion!",
+            "What is this? A center for ants?",
+            "I'm not a baby, I am a man! I am an anchorman!",
+            "I look good. I mean, really good. Hey everyone! Come and see how good I look!",
+            "You're so wise. You're like a miniature Buddha, covered in hair."
+        ]
+        elif assistant == "Zoolander":
+            catch_phrases = [
+                "What is this? A center for ants?",
+                "I'm pretty sure there's a lot more to life than being really, really, ridiculously good looking.",
+                "I feel like I'm taking crazy pills!",
+                "Blue Steel!",
+                "Magnum!",
+                "Moisture is the essence of wetness, and wetness is the essence of beauty.",
+                "So hot right now!",
+                "He's absolutely right.",
+                "I'm not an ambi-turner.",
+                "It's a walk-off!",
+                "Obey my dog!",
+                "I invented the piano key necktie!",
+                "Who you tryin' to get crazy with, ese? Don't you know I'm loco?",
+                "Listen to your friend Billy Zane. He's a cool dude."
+            ]
+        else:
+            raise
+            catch_phrases = ["No catch phrases available for this character."]
+        
+        sys_msg = f"""You are a helpful chatbot that answers the user's questions about Verizon phone plans.
+        You will respond with the personality of {assistant}.
+        Remember to always return your result in the JSON format, or else I won't be able to understand you!.
+        Do not repeat information.
+        Do not use the name of your tools, I dont want to hear them.
+        Make your reponses as short as possible, being effective at getting to the point while providing the necessary info.
+        Use catch phrases: {', '.join(catch_phrases)} but don't overuse them, be creative and clever!
+        If the user is ready to checkout ask them for their name and email address.  Remember to use the JSON format for this. There is no tool for this.
+        """
+
+        prompt = conversational_agent.agent.create_prompt(
+        system_message=sys_msg,
+        tools=tools
+        )
+        conversational_agent.agent.llm_chain.prompt = prompt
+        #session['agent'] = conversational_agent
+        
+        #session['agent']("Introduce yourself and ask me if I am a returning Verizon customer or not.")
+        session['test'] = 1
+
+        return {"role": assistant, "content": 1}, 200
 
 import time
 @app.route('/api/stream')
@@ -312,23 +225,17 @@ def generate_stream():
 def chatbox():
     #get query string
     prompt = request.json.get('prompt')
+    assistant = request.json.get('assistant')
 
     if prompt is None or not isinstance(prompt, str):
         return jsonify(error="Missing or invalid prompt"), 400
     print(prompt)
 
-    if 'agent' not in session:
-        #initialize agent here
-        #session['agent'] = initialize_agent()
-        session['agent'] = "agent" #place holder
+    #session['agent'](prompt)
+    print(session['test'])
 
-        #agent will make first response
-        
-    else:
-        agent = session['agent']
-        agent(prompt)
 
-    return jsonify(prompt=prompt), 200
+    return {"role": assistant, "content": prompt}, 200
 
 
 
