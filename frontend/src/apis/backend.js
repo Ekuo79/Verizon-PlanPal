9e9c09a73addb94c116afaac4fe981b6fc6a02b1
Luke Li <luke@lukexli.com>
2023-10-15 05:20:03 -0400

    feat: chat dialog

--- frontend/src/apis/backend.js
+++ frontend/src/apis/backend.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default axios.create({
   baseURL: process.env.REACT_APP_SERVER_HOST + process.env.REACT_APP_SERVER_API_PATH,
   headers: { GATORAPPS_app: process.env.REACT_APP_APP_NAME },
+  withCredentials: true
 });
 
 export const axiosPrivate = axios.create({
--- frontend/src/views/Chat/Chat.js
+++ frontend/src/views/Chat/Chat.js
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef, Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { Box, Button, Container, Paper, TextField, Typography } from '@mui/material';
+import { Avatar, Box, Button, Container, Divider, List, ListItem, ListItemAvatar, ListItemText, Paper, TextField, Typography } from '@mui/material';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import SkeletonGroup from '../../components/SkeletonGroup/SkeletonGroup';
@@ -12,6 +12,7 @@ const Chat = () => {
   const navigate = useNavigate();
   const [message, setMessage] = useState('');
   const [chatHistory, setChatHistory] = useState([]);
+  const chatHisRef = useRef(null);
 
   const assistant = useSelector((state) => state.chat.assistant);
 
@@ -25,13 +26,12 @@ const Chat = () => {
   };
 
   const handleSendMessage = async () => {
-    if (message.trim()) {
-      setChatHistory([...chatHistory, message]);
+    const prompt = message.trim();
+    if (prompt) {
       setMessage('');
       try {
-        const response = await axios.post('/chat', {
-          prompt: message.trim()
-        });
+        const response = await axios.post('/chat', { assistant, prompt });
+        setChatHistory([...chatHistory, { 'role': 'User', content: prompt }, response?.data]);
       } catch (error) {
       }
     }
@@ -46,6 +46,11 @@ const Chat = () => {
     handleSetAssistant();
   }, []);
 
+  useEffect(() => {
+    const element = chatHisRef.current;
+    element.scrollTop = element.scrollHeight;
+  }, [chatHistory]);
+
   return (
     <div className='GenericPage'>
       <Header />
@@ -55,11 +60,11 @@ const Chat = () => {
             <Box className="GenericPage__container_title_box GenericPage__container_title_flexBox GenericPage__container_title_flexBox_left">
               <Box className="GenericPage__container_title_flexBox GenericPage__container_title_flexBox_left">
                 <Typography variant="h1">{`Chat with ${assistant}`}</Typography>
-                <Button onClick={() => handleSwitchAssistant()} size="medium" sx={{ 'margin-left': '16px' }}>Switch</Button>
+                <Button onClick={() => handleSwitchAssistant()} size="medium" sx={{ 'margin-left': '23px' }}>Switch</Button>
               </Box>
               <Box className="GenericPage__container_title_flexBox GenericPage__container_title_flexBox_right" sx={{ 'flex-grow': '1' }}>
                 <Box className="GenericPage__container_title_flexBox_right">
-                  <Button variant="contained" size="medium">Button</Button>
+                  {/* <Button variant="contained" size="medium">Button</Button> */}
                 </Box>
               </Box>
             </Box>
@@ -79,41 +84,55 @@ const Chat = () => {
                       display: 'flex',
                       flexDirection: 'column',
                       height: '75vh',
-                      maxWidth: '600px',
+                      maxWidth: '700px',
                       margin: '0 auto',
-                      p: 2
+                      paddingTop: '20px'
                     }}
                   >
                     {/* Chat History */}
                     <Box
-                      component={Paper}
+                      ref={chatHisRef}
                       sx={{
                         flexGrow: 1,
                         overflowY: 'auto',
-                        p: 2,
-                        mb: 2
+                        p: 2
                       }}
                     >
-                      {chatHistory.map((msg, index) => (
-                        <Box key={index} sx={{ mb: 1 }}>
-                          {msg}
-                        </Box>
-                      ))}
+                      <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                        {chatHistory.map((msg, index) => {
+                          return (
+                            <Box key={index} sx={{ bgcolor: msg.role === 'User' ? 'white' : 'rgb(247, 247, 248)' }}>
+                              <ListItem alignItems="flex-start" sx={{ width: '100%' }}>
+                                <ListItemAvatar>
+                                  <Avatar src={msg.role === 'User' ? `` : `https://verizon-chatbot-dev.s3.us-east-1.amazonaws.com/${msg.role.replace(/\s+/g, '').toLocaleLowerCase()}.jpg`} />
+                                </ListItemAvatar>
+                                <ListItemText
+                                  primary={
+                                    <Typography sx={{ wordWrap: 'break-word', overflowWrap: 'break-word', lineHeight: '1.4' }} >
+                                      {msg.content}
+                                    </Typography>
+                                  }
+                                />
+                              </ListItem>
+                              <Divider variant="inset" component="li" />
+                            </Box>
+                          )
+                        })}
+                      </List>
                     </Box>
 
                     {/* Input Area */}
                     <Box
-                      component={Paper}
                       sx={{
-                        p: 2,
                         display: 'flex',
-                        alignItems: 'center'
+                        alignItems: 'center',
+                        marginBottom: '20px'
                       }}
                     >
                       <TextField
                         fullWidth
                         variant="outlined"
-                        placeholder="Type a message..."
+                        placeholder="Send a message"
                         value={message}
                         onChange={(e) => setMessage(e.target.value)}
                         onKeyPress={(e) => {
